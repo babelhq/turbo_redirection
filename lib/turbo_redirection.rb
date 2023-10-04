a5b195d5ff052e94b20de7a15e834900cf0439cf
Igor Malinovskiy <ihor.malinovskiy@gmail.com>
2023-10-04 15:20:40 +0300

    Require ActiveSupport

--- lib/turbo_redirection.rb
+++ lib/turbo_redirection.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "active_support/concern"
 require_relative "turbo_redirection/version"
 require_relative "turbo_redirection/break_out_of_frame"
 require_relative "turbo_redirection/recede_on_web"
